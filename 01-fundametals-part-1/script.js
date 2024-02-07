8c2685dc8ef14629437e06a8cd2a475d93ce3521
Dmytro Melnyk <u1131057@internal.imsglobal.com>
2024-02-07 18:32:27 +0200

    fundamentals part 1

---
+++ 01-fundametals-part-1/script.js
@@ -0,0 +1,91 @@
+
+// Link file
+// let js = "amazing";
+// if (js === "amazing") alert("JS IS FUN");
+
+// Values and Variables
+// let country = "Ukraine";
+// let continent = "Europe";
+// let population = 40;
+//
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// Data Types
+// let country = "Ukraine";
+// let isIsland = false;
+// let language;
+//
+// console.log(typeof isIsland);
+// console.log(typeof language);
+// console.log(typeof country);
+
+// let, const and var
+// let language = 'ukrainian';
+// const gravity = 9.8;
+// gravity = 1;
+
+// Basic Operators
+// let population = 40;
+// let firstResult = (population / 2) / 2;
+// console.log(firstResult)
+// let description = ''
+
+// Coding Challenge #1
+// const markHeight = 1.69;
+// const johnHeight = 1.95;
+//
+// const markMass = 78;
+// const johnMass = 92;
+//
+// function calculateBMI(height, mass) {
+//     return mass / (height * height)
+// }
+//
+// let markHigher = markHeight > johnHeight;
+// let markBMI = calculateBMI(markHeight, markMass);
+// let johnBMI = calculateBMI(johnHeight, johnMass);
+//
+// console.log('is mark Higher: ' + markHigher);
+// console.log('mark BMI: ' + markBMI)
+// console.log('john BMI: ' + johnBMI)
+//
+// if (markBMI > johnBMI) {
+//     console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+// } else {
+//     console.log("John's BMI is higher than Mark's!");
+// }
+
+// Strings and Template Literals
+// const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+
+// Coding Challenge #3
+// let dolphinsScore = [96, 108, 89];
+// let koalasScore = [88, 91, 110];
+//
+// function calcAvg(arrayOfNumbers) {
+//     let sum = 0;
+//     arrayOfNumbers.forEach((num) => sum = sum + num);
+//     return sum / arrayOfNumbers.length;
+// }
+//
+// let dolphinsAvg = calcAvg(dolphinsScore);
+// let koalasAvg = calcAvg(koalasScore);
+//
+// if (dolphinsAvg > koalasAvg && dolphinsAvg >= 100) {
+//     console.log('dolphins win')
+// } else if (koalasAvg > dolphinsAvg && koalasAvg >= 100) {
+//     console.log('koalas win')
+// } else {
+//     console.log('no one win')
+// }
+
+// Coding Challenge #4
+
+// let bill = 275;
+//
+// let tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.2;
+//
+// console.log('The bill was ' + bill + ', the tip was ' + tip + ', and the total value ' + (tip + bill));
+
